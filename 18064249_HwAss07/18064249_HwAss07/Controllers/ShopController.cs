44397a5ef583dc437416c7de259203c7ddc95b91
Le-Feu <u18064249@tuks.co.za>
2019-07-17 19:31:19 +0200

    Second Push!

--- 18064249_HwAss07/18064249_HwAss07/Controllers/ShopController.cs
+++ 18064249_HwAss07/18064249_HwAss07/Controllers/ShopController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _18064249_HwAss07.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,16 @@ namespace _18064249_HwAss07.Controllers
     public class ShopController : Controller
     {
         // GET: Shop
+        [HttpPost]
         public ActionResult Index()
         {
+            return View();
+        }
+        public ActionResult AddItem(string NameOfItem,string description,double price,int QuantityNumber)
+        {
+            List<ShowItemViewModel> newItem = new List<ShowItemViewModel>();
+            ShowItemViewModel item = new ShowItemViewModel(NameOfItem, description, price, QuantityNumber);
+            newItem.Add(item);
             return View();
         }
     }
